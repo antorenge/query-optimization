cb77fca1e09374037bbe42be9ff7f8d5a6c9575c
Antony Orenge <antoroko@gmail.com>
2018-10-12 21:53:04 +0300

    Add test for report query and update search query test

--- src/test/java/io/fortumo/AppTest.java
+++ src/test/java/io/fortumo/AppTest.java
@@ -20,11 +20,12 @@ public class AppTest {
     public void test_that_it_starts() throws Exception {
         final App app = new App(new String[]{});
         app.start(false);
-        this.testQuery();
+        this.testSearchQuery();
+        this.testReportQuery();
         app.stop();
     }
 
-    private void testQuery() throws IOException {
+    private void testSearchQuery() throws IOException {
         final String testUrl = "http://localhost:12000/?country=EE";
         final URLConnection con = new URL(testUrl).openConnection();
         final String someContent = new BufferedReader(
@@ -33,4 +34,15 @@ public class AppTest {
         assertThat(someContent).isNotEmpty();
         System.out.println("DB Search Result " + someContent);
     }
+
+    private void testReportQuery() throws IOException {
+        final String testUrl = "http://localhost:12000/report/?merchant=849961a24d5dc6d961da43e550a2f0b9&start_date=2018-10-10&end_date=2018-10-10";
+        final URLConnection con = new URL(testUrl).openConnection();
+        final String someContent = new BufferedReader(
+                new InputStreamReader(con.getInputStream())
+        ).lines().collect(Collectors.joining("\n"));
+        assertThat(someContent).isNotEmpty();
+        System.out.println("DB Report Result " + someContent);
+    }
+
 }
